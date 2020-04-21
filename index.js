549513b9d4cfaa4b3a792b01ed98a6ba97cb0847
Tuan Nguyen <tuannm072090@gmail.com>
2020-04-21 16:18:18 +0700

    config notification for android

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 
+//  read more here https://rnfirebase.io/messaging/usage#background-application-state
+function HeadlessCheck({ isHeadless }) {
+    if (isHeadless) {
+        // App has been launched in the background by iOS, ignore
+        return null;
+    }
+
+    return <App />;
+}
+
 // Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('handled in the background when app close and not kill...', remoteMessage);
+    console.log('handled in the background when app close and kill...', remoteMessage);
 });
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
